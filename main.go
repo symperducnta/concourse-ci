402ce4f753039f76d8933ab0681ecd6b9ac48f15
ducngo <ducnta@symper.vn>
2025-04-29 16:11:27 +0700

    feat: add http server with gin

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	log.Println("Hello world")
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run()
 }
